7cd174b008ddb11419438d4cedc6f92b729fbb62
GitHub BaoPaper <GitHub baopaper@163.com>
2023-08-13 22:09:06 +0800

    修复天气显示

--- sys_info.py
+++ sys_info.py
@@ -150,7 +150,7 @@ def main():
                # 绘制分隔线
                draw.line((0, top, width, top), fill=255)
                # top += 12
-               draw.text((0, top), "豹纸嗷呜", font=font_small, fill=255)
+               draw.text((0, top), result, font=font_small, fill=255)
                pass
             else:
                current_time = datetime.datetime.now().strftime("%H:%M:%S")
